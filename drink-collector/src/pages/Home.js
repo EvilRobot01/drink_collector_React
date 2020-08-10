355f866eb59339769cad1d1aeff1ad1dab3adc5b
HaroldDRF <95robots@gmail.com>
2020-08-10 00:37:01 -0700

    home page setup

--- drink-collector/src/pages/Home.js
+++ drink-collector/src/pages/Home.js
@@ -4,8 +4,39 @@ import SearchForm from '../components/SearchForm';
 
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
-  const [searchTerm, setSearchTerm] = React.useState('active');
+  const [searchTerm, setSearchTerm] = React.useState('defefe');
   const [cocktails, setCocktails] = React.useState([]);
+
+  React.useEffect(()=>{
+    setLoading(true);
+    async function getDrinks(){
+      try{
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+        const data = await response.json();
+        const {drinks} = data;
+        if(drinks){
+          const newCocktails = drinks.map(item=>{
+          const {idDrink, strDrink, strDrinkThumb, strAlcholic, strGlass} = item;
+          return {id:idDrink, 
+                  name:strDrink,
+                  image: strDrinkThumb,
+                  info: strAlcholic,
+                  glass: strGlass
+                }
+          });
+          setCocktails(newCocktails);
+        }else{
+          setCocktails([]);
+        }
+      }catch (error){
+
+      }
+      setLoading(false);
+    }
+    getDrinks();
+  }, [searchTerm]);
+
+
 
   return (<main>
     <SearchForm setSearchTerm={setSearchTerm} />
