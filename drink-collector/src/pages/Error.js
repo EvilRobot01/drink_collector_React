d28c8befdf1d615b392a6788a0769e73b9c86ada
HaroldDRF <95robots@gmail.com>
2020-08-11 10:22:10 -0700

    final project

--- drink-collector/src/pages/Error.js
+++ drink-collector/src/pages/Error.js
@@ -1,13 +1,12 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 export default function Error() {
   return (
     <section className="error-page section">
-      <div className='error-container'>
-        <h1>this is a dead end</h1>
-        <Link to="/" className='btn btn-primary'>
-          Back home
+      <div className="error-container">
+        <h1>oops! it's a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
         </Link>
       </div>
     </section>
--- drink-collector/src/pages/Home.js
+++ drink-collector/src/pages/Home.js
@@ -1,46 +1,54 @@
 import React from "react";
-import CocktailsList from '../components/CocktailList';
-import SearchForm from '../components/SearchForm';
-
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 export default function Home() {
   const [loading, setLoading] = React.useState(false);
-  const [searchTerm, setSearchTerm] = React.useState('defefe');
+  const [searchTerm, setSearchTerm] = React.useState("a");
   const [cocktails, setCocktails] = React.useState([]);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     setLoading(true);
-    async function getDrinks(){
-      try{
-        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
         const data = await response.json();
-        const {drinks} = data;
-        if(drinks){
-          const newCocktails = drinks.map(item=>{
-          const {idDrink, strDrink, strDrinkThumb, strAlcholic, strGlass} = item;
-          return {id:idDrink, 
-                  name:strDrink,
-                  image: strDrinkThumb,
-                  info: strAlcholic,
-                  glass: strGlass
-                }
+        const { drinks } = data;
+        if (drinks) {
+          const newCocktails = drinks.map(item => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcoholic,
+              strGlass
+            } = item;
+
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass
+            };
           });
           setCocktails(newCocktails);
-        }else{
+        } else {
           setCocktails([]);
         }
-      }catch (error){
-
+      } catch (error) {
+        console.log(error);
       }
       setLoading(false);
     }
+
     getDrinks();
   }, [searchTerm]);
-
-
-
-  return (<main>
-    <SearchForm setSearchTerm={setSearchTerm} />
-    <CocktailsList loading={loading} cocktails={cocktails}/>
-  </main>
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList cocktails={cocktails} loading={loading} />
+    </main>
   );
 }
